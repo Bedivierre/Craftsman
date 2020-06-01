f399c4c4046b8fcaa7907c97ffbb8b1d04015017
Bedivierre <bookvariant@list.ru>
2020-06-01 11:20:26 +0300

    First commit

---
+++ src/DataTransport/BaseResponseObject.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace Bedivierre\Craftsman\Joiner;
+use Bedivierre\Craftsman\Carpenter\BaseDataObject;
+
+/**
+ * Class BaseResponseObject
+ * @package Bedivierre\Sberbank\Base
+ */
+class BaseResponseObject extends BaseDataObject
+{
+    const ERR_OK = 0;
+    const ERR_UNKNOWN = 1;
+
+    /**
+     * @param string|array $data Тело ответа, как правило, строка JSON
+     * @param string $url URL, по которому был совершен запрос.
+     * @param string $method Метод запроса (POST или GET)
+     */
+    public function __construct($data, string $url = '', string $method = 'post')
+    {
+        $this->_url = $url;
+        $methods = ['post', 'get'];
+        if(in_array(strtolower($method), $methods))
+            $this->_method = $method;
+        else
+            $this->_method = $methods[0];
+        $this->_errorMessage = '';
+        $this->_errorCode = 0;
+        parent::__construct($data);
+    }
+
+    /**
+     * Метод отправки запроса (POST или GET)
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->_method;
+    }
+    /**
+     * URL запроса, от кторого получен данный ответ.
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->_url;
+    }
+
+    /**
+     * Сообщение ошибки
+     * @return string
+     */
+    public function errorMessage()
+    {
+        return $this->_errorMessage;
+    }
+    /**
+     * Код ошиибки
+     * @return int
+     */
+    public function errorCode()
+    {
+        return $this->_errorCode;
+    }
+    /**
+     * Определяет, есть ли ошибка в ответе.
+     * @return bool
+     */
+    public function hasError()
+    {
+        return $this->_errorCode != self::ERR_OK;
+    }
+    /**
+     * Установить код ошиибки.
+     * @param string $message Сообщение ошибки.
+     * @param int $err_code Код ошибки. По умолчанию равен BaseResponseObject::ERR_UNKNOWN
+     */
+    public function setError(string $message, int $err_code = self::ERR_UNKNOWN)
+    {
+        $this->_errorMessage = $message;
+        $this->_errorCode = $err_code;
+    }
+
+}
---
+++ src/Utility.php
@@ -0,0 +1,120 @@
+<?php
+namespace Bedivierre\Craftsman;
+
+class Utility
+{
+    /**
+     * Функция берет элемент массива по его имени, включая вложенные элементы. Разделителем пути по умолчанию
+     * являтся '.'
+     * @param array $array Массив, из которого нужно получить данные.
+     * @param string $path Имя необходимого элемента.
+     * @param string $limiter Разделитель для пути.
+     * @return array|mixed|null
+     */
+    public static function arr_path(array &$array, string $path, string $limiter = '.')
+    {
+        if (!is_array($array))
+            return null;
+        $p = explode($limiter, $path);
+        if (!$p)
+            return $array;
+        $count = count($p);
+        $counter = 1;
+        $var = $array;
+        foreach ($p as $_p) {
+            if ($count == $counter) {
+                if (isset($var[$_p]))
+                    return $var[$_p];
+                else
+                    return null;
+            }
+            if (!isset($var[$_p]) || !is_array($var[$_p]))
+                return null;
+            $var = $var[$_p];
+            $counter++;
+        }
+        return null;
+    }
+    /**
+     * @param $url
+     * @param $data
+     * @return BaseResponseObject|null
+     */
+    public static function post($url, $data){
+        $query = http_build_query($data);
+        $ch = curl_init();
+        $defaults = array(
+            CURLOPT_URL => $url,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $query,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER=>true,
+        );
+        curl_setopt_array($ch, $defaults);
+
+// загрузка страницы и выдача её браузеру
+        $json = curl_exec($ch);
+
+        if(curl_error($ch))
+        {
+            curl_close($ch);
+            return self::createErrorResponse('Ошибка при запросе: ' . curl_error($ch), $url);
+        }
+        curl_close($ch);
+        return new BaseResponseObject($json, $url, 'post');
+    }
+
+    /**
+     * @param $url
+     * @param $data
+     * @return BaseResponseObject|null
+     */
+    public static function get($url, $data){
+        $query = http_build_query($data);
+        $ch = curl_init();
+        $uri = $url."?".$query;
+        $defaults = array(
+            CURLOPT_URL => $uri,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER=>true,
+        );
+        curl_setopt_array($ch, $defaults);
+
+// загрузка страницы и выдача её браузеру
+        $json = curl_exec($ch);
+        if(curl_error($ch))
+        {
+            curl_close($ch);
+            return self::createErrorResponse('Ошибка при запросе: ' . curl_error($ch), $uri);
+        }
+        curl_close($ch);
+        return new BaseResponseObject($json, $uri, 'get');
+    }
+
+    /**
+     * Возвращает новый объект ответа с ошибкой.
+     * @param string $text Текст ошибки
+     * @param string $url URL запроса, вернувшего ошибку.
+     * @return BaseResponseObject
+     */
+    public static function createErrorResponse(string $text, string $url)
+    {
+        $r = new BaseResponseObject('', $url);
+        $r->setError($text);
+        return $r;
+    }
+
+
+    public static function runRequest(BaseRequestObject $request)
+    {
+        if($request->getMethod() == 'post') {
+            return self::post($request->getHost(), $request->buildQuery());
+        }elseif ($request->getMethod() == 'get'){
+            return self::get($request->getHost(), $request->buildQuery());
+        } else {
+            return self::createErrorResponse(
+                "Неверный метод отправки данных :".$request->getMethod(),
+                $request->getHost());
+        }
+    }
+}
