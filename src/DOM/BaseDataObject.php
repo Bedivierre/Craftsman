bc30ba5727f7789a19ae725c225587e9d9f92d59
Bedivierre <bookvariant@list.ru>
2020-06-03 10:44:25 +0300

    Поправка процесса содания базового объекта

--- src/DOM/BaseDataObject.php
+++ src/DOM/BaseDataObject.php
@@ -184,17 +184,11 @@ class BaseDataObject implements \Iterator, \ArrayAccess
     public function apply_data($data, $overwrite = true, $newdata = false)
     {
         $arr =  $this->getArrayFromInputData($data);
-        if(!$arr || !is_array($arr) || !($data instanceof BaseDataObject))
+        if(!$arr || !is_array($arr))
             return;
 
         if($newdata)
             $this->_data = [];
-        if($data instanceof BaseDataObject){
-            $this->absorb($data, true);
-            return;
-        }
-
-
         foreach ($arr as $key => $value){
             $this->addMember($key, $value, true, $overwrite);
         }
@@ -206,8 +200,10 @@ class BaseDataObject implements \Iterator, \ArrayAccess
      */
     protected function getArrayFromInputData($data)
     {
-        if(!is_string($data) && !is_array($data))
+        if(!is_string($data) && !is_array($data) && !($data instanceof BaseDataObject))
             return null;
+        if($data instanceof BaseDataObject)
+            return $data->toArray();
         return is_array($data) ? $data : json_decode($data, true);
     }
 
